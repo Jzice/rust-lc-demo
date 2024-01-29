dbbdbb8922d92927f6d2cdac4dbfe13c27c807c4
Justice <zhengyi.zhu.hust@gmail.com>
2024-01-29 11:31:30 +0800

    update leetcode

--- src/leetcode/1.两数之和.rs
+++ src/leetcode/1.两数之和.rs
@@ -1,23 +1,23 @@
-/*
+/*!
  * @lc app=leetcode.cn id=1 lang=rust
  *
- * [1] 两数之和
+ * # [1] 两数之和
  *
  * https://leetcode.cn/problems/two-sum/description/
  *
  * algorithms
- * Easy (47.11%)
- * Likes:    7621
- * Dislikes: 0
- * Total Accepted:    814.7K
- * Total Submissions: 1.7M
- * Testcase Example:  '[2,7,11,15]\n9'
+ * * Easy (47.11%)
+ * * Likes:    7621
+ * * Dislikes: 0
+ * * Total Accepted:    814.7K
+ * * Total Submissions: 1.7M
+ * * Testcase Example:  '[2,7,11,15]\n9'
  *
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  *
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
  *
- * 示例:
+ * ## 示例:
  *
  * 给定 nums = [2, 7, 11, 15], target = 9
  *
@@ -27,12 +27,11 @@
  *
  */
 
-/// 两数之和
-struct Solution;
+use super::*;
 
 // @lc code=start
 impl Solution {
-    /// # [1] 两数之和
+    /// # 1.两数之和
     /// ## 解题思路
     /// - hashmap
     /// 1. 遍历序列；
@@ -56,6 +55,7 @@ impl Solution {
     }
 }
 // @lc code=end
+//
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -1,4 +1,21 @@
 //! # leetcode代码库
 //!
 
+
 automod::dir!(pub "src/leetcode");
+pub struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
